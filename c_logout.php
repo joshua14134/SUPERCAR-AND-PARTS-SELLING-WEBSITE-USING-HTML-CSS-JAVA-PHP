8237cc63fd5d132a700a4d43b782bbf6586a3eca
JOSHUA <164365977+joshua14134@users.noreply.github.com>
2026-02-12 22:30:29 +0530

    Delete c_logout.php
    Signed-off-by: JOSHUA  <164365977+joshua14134@users.noreply.github.com>

--- c_logout.php
+++
@@ -1,8 +0,0 @@
-<?php
-session_start(); // Start the session
-session_unset(); // Unset all session variables
-session_destroy(); // Destroy the session
-
-header("Location: c_login.php"); // Redirect to login page
-exit();
-?>
